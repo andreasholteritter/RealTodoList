3c6ded5a054e5ccb810b8e15cde41c9ba34faa7c
Andreas Ritter <ritand16@student.westerdals.no>
2018-02-28 13:07:32 +0100

    first commit from new project with second ToDoList

---
+++ src/models/todo.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 14/02/2018.
+ */
+export interface ToDo {
+  id?: string;
+  name: string;
+  done: boolean;
+}
---
+++ src/pages/authorize/authorize.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AngularFirestore} from "angularfire2/firestore";
+
+@IonicPage()
+@Component({
+  selector: 'page-authorize',
+  templateUrl: 'authorize.html',
+})
+export class AuthorizePage {
+
+  public user = {
+    username: null,
+    password: null
+  } as User;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private af: AngularFirestore) {
+  }
+
+  loginUser() {
+    this.af.app.auth()
+      .signInWithEmailAndPassword(this.user.username, this.user.password)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  registerUser() {
+    this.af.app.auth()
+      .createUserWithEmailAndPassword(this.user.username, this.user.password)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AuthorizePage');
+  }
+
+}
+
+interface User {
+  username: string;
+  password: string;
+}
