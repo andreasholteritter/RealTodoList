0b1643a2d70ec03bbaaabe2a344c3a644158f238
Andreas Ritter <ritand16@student.westerdals.no>
2018-02-28 13:44:23 +0100

    test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,40 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { ToDo } from '../../models/todo';
 
-/**
- * Generated class for the HomePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html',
+  templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public collection: AngularFirestoreCollection<ToDo>;
+  public posts: Observable<ToDo[]>;
+
+  constructor(
+    public navCtrl: NavController,
+    private af: AngularFirestore) {
+
+    this.collection = af.collection<ToDo>("todos");
+    this.posts = this.collection.snapshotChanges()
+      .map(actions =>  {
+        return actions.map(action => {
+          let data = action.payload.doc.data() as ToDo;
+          let idt = action.payload.doc.id;
+
+          return {
+            idt,
+            ...data
+          };
+        })
+      });
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+  logout() {
+    this.af.app.auth().signOut();
   }
 
 }
